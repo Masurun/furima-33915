774175f23a6d8625118d600a9d9c4d3243964797
Masurun <wataru.5756@gmail.com>
2021-01-31 21:42:39 +0900

    ユーザー管理機能実装（deviseの導入、ユーザー管理機能実装、モデルの単体テストコード）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth 
-
-
+  
+  before_action :configure_permitted_parameters, if: :devise_controller?
   private
 
   def basic_auth
@@ -9,5 +9,8 @@ class ApplicationController < ActionController::Base
       # username  =='wataru'  &&  password  == '1223'
       username == ENV["BASIC_AUTH_USER"]  && password  == ENV["BASIC_AUTH_PASSWORD"]
     end
+  end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,keys:[:nickname,:birth,:first_name,:last_name,:first_name_kana,:last_name_kana])
   end
 end
