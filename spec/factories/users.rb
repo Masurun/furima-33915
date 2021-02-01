e948e721ec6183e9781e9ba1ac3f10f0fc8b6a7b
Masurun <wataru.5756@gmail.com>
2021-02-01 13:02:55 +0900

    Fakerを利用したテストコードの記述、テストコード可読性の向上、emailのテストケースの追加、パスワードのテストケースの追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :user do
     nickname                  { 'test' }
-    email                     { 'test@test.com' }
-    password                  { 'a11111' }
+    email                     { Faker::Internet.free_email }
+    password                  { Faker::Internet.password(min_length: 6) }
     password_confirmation     { password }
     first_name                { '山田' }
     last_name                 { '太郎' }
