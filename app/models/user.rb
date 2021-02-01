664b4d0eff11d3c84e2effe50e469c585ae0748a
Masurun <wataru.5756@gmail.com>
2021-02-01 14:27:06 +0900

    emailのダミーの修正、バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, presence: true
-  validates :birth, presence: true
 
+  with_options presence: true do
+    validates :nickname
+    validates :birth
+  end
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ } do
     validates :first_name
     validates :last_name
