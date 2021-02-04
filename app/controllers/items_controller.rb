f442fdf488e11ecec1e757032542ab5555fd4774
Masurun <wataru.5756@gmail.com>
2021-02-04 12:38:17 +0900

    destroyコントローラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!,except:[:index,:show]
-  before_action :set_item,only:[:show]
+  before_action :set_item,only:[:show,:destroy]
   def index
   @items=Item.includes(:user).order("created_at DESC")
   end
@@ -22,9 +22,10 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item=Item.find(params[:id])
-    item.destroy
+    if user_signed_in? && @item.user.id==current_user.id
+    @item.destroy
     redirect_to root_path
+    end
   end
   private
 
